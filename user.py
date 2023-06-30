0efc365e486d63607c4f4166d1dacca44e603361
cacigev <carl_simon.schultheis@tu-darmstadt.de>
2023-06-30 12:14:43 +0200

    Added async queue related to #2
    Added async queue to control.py. At the moment the termination of threads is not working.

--- user.py
+++ user.py
@@ -42,16 +42,16 @@ logging.basicConfig(
 # wm0 = control.Wavemeter(4734, dll_path="./wmControl/wlmData.dll")  # Quips C 192.168.1.45
 # wm1 = control.Wavemeter(536)  # Quips B 192.168.1.240
 if sys.platform == "win32":
-    wm2 = control.Wavemeter(4711, dll_path="./wmControl/wlmData.dll")  # Quips B 192.168.1.240
+    wm2 = control.Wavemeter(4711, dll_path="./wmControl/wlmData.dll", start_main=True)  # Quips B 192.168.1.240
 elif sys.platform == "linux":
-    wm2 = control.Wavemeter(4711, dll_path="./wmControl/libwlmData.so")
+    wm2 = control.Wavemeter(4711, dll_path="./wmControl/libwlmData.so", start_main=True)
 
 
 # wm1.wavelengths(1)
 # wm2.wavelengths(1)
 # wm1.wavelengths(1)
 # wm1.frequencys(1)
-wm2.frequencys(1)
+# wm2.frequencys(1)
 # wm1.frequencys(1)
 # wm0.wavelengths(1)
 # wm0.frequencys(1)
--- wmControl/thread.py
+++ wmControl/thread.py
@@ -1,11 +1,44 @@
 import logging
+import janus
+import asyncio
+import time
+import ctypes
 
 from wmControl import control, wlmConst, wlmData
 
 
 class Callback:
     version = 0
-    put = None
+    queue = None
+
+    def callback(self, sync_q: janus.SyncQueue[int]) -> None:
+
+        def helper(ver: int, mode: int, int_val: int, double_val: float, result: int):
+            sync_q.put(f"Time:{int_val}, WM:{ver}, Channel:{mode}, Wavelength:{double_val:.8f}, Res1:{result}")
+
+        callbacktype = ctypes.CFUNCTYPE(None,
+            ctypes.c_int32,
+            ctypes.c_int32,
+            ctypes.c_int32,
+            ctypes.c_double,
+            ctypes.c_int32,
+        )
+        callbackpointer = callbacktype(helper)
+
+        try:
+            while "not terminated":
+                wlmData.dll.Instantiate(
+                    wlmConst.cInstNotification,
+                    wlmConst.cNotifyInstallCallbackEx,
+                    callbackpointer,
+                    0,
+                )
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt: Thread will be terminated.")
+        finally: wlmData.dll.Instantiate(wlmConst.cInstNotification, wlmConst.cNotifyRemoveCallback, -1, 0)
+
+        sync_q.join()
 
     # Prints all measured frequencies of one WM
     # Unit: THz
@@ -43,58 +76,6 @@ class Callback:
                     299792.458 / double_val,
                 )
 
-    # Prints all measured wavelengths one WM
-    # Unit: nm
-    def wavelengthsProcEx(self, ver: int, mode: int, int_val: int, double_val: float, result: int):
-        const_to_channel = {
-            wlmConst.MeasureMode.cmiWavelength1: 1,
-            wlmConst.MeasureMode.cmiWavelength2: 2,
-            wlmConst.MeasureMode.cmiWavelength3: 3,
-            wlmConst.MeasureMode.cmiWavelength4: 4,
-            wlmConst.MeasureMode.cmiWavelength5: 5,
-            wlmConst.MeasureMode.cmiWavelength6: 6,
-            wlmConst.MeasureMode.cmiWavelength7: 7,
-            wlmConst.MeasureMode.cmiWavelength8: 8,
-        }
-
-        if (ver == self.version) and (mode in const_to_channel):
-            print(f"Time:{int_val}, WM:{ver}, Channel:{const_to_channel[mode]}, Wavelength:{double_val:.8f}")
-
-    # Prints all measured wavelengths of all WMs connected to the control-PC
-    # Unit: nm
-    def allwavelengthsProcEx(self, Ver, Mode, IntVal, DblVal, Res1):
-        if Mode == wlmConst.MeasureMode.cmiWavelength1:
-            # print("Time:{}, WM:{}, Channel:1, Wavelength:{:.8f}".format(IntVal, Ver, DblVal))
-            self.put([IntVal, Ver, DblVal])
-        if Mode == wlmConst.MeasureMode.cmiWavelength2:
-            # print("Time:{}, WM:{}, Channel:2, Wavelength:{:.8f}".format(IntVal, Ver, DblVal))
-            self.put([IntVal, Ver, DblVal])
-        if Mode == wlmConst.MeasureMode.cmiWavelength3:
-            # print("Time:{}, WM:{}, Channel:3, Wavelength:{:.8f}".format(IntVal, Ver, DblVal))
-            self.put([IntVal, Ver, DblVal])
-        if Mode == wlmConst.MeasureMode.cmiWavelength4:
-            # print("Time:{}, WM:{}, Channel:4, Wavelength:{:.8f}".format(IntVal, Ver, DblVal))
-            self.put([IntVal, Ver, DblVal])
-        if Mode == wlmConst.MeasureMode.cmiWavelength5:
-            # print("Time:{}, WM:{}, Channel:5, Wavelength:{:.8f}".format(IntVal, Ver, DblVal))
-            self.put([IntVal, Ver, DblVal])
-        if Mode == wlmConst.MeasureMode.cmiWavelength6:
-            # print("Time:{}, WM:{}, Channel:6, Wavelength:{:.8f}".format(IntVal, Ver, DblVal))
-            self.put([IntVal, Ver, DblVal])
-        if Mode == wlmConst.MeasureMode.cmiWavelength7:
-            # print("Time:{}, WM:{}, Channel:7, Wavelength:{:.8f}".format(IntVal, Ver, DblVal))
-            self.put([IntVal, Ver, DblVal])
-        if Mode == wlmConst.MeasureMode.cmiWavelength8:
-            # print("Time:{}, WM:{}, Channel:8, Wavelength:{:.8f}".format(IntVal, Ver, DblVal))
-            self.put([IntVal, Ver, DblVal])
-
-    def getSwitchedChannel(self, Ver, Mode, IntVal, DblVal, Res1):
-        if Ver == self.version and Mode == wlmConst.MeasureMode.cmiSwitcherChannel:
-            print(
-                f"Time:{Res1}, WM:{Ver}, Active Channel: {IntVal}, Getter-state:{DblVal} Wavelength:{wlmData.dll.GetWavelengthNum(IntVal, 0):.8f}"
-            )
-
     def __init__(self, ver, wavemeter):
         self.__logger = logging.getLogger(__name__)
         self.version = ver
-        # self.put = wavemeter.putBfr
