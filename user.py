8596c9242f6b0030c9f93c032fdce6fba6a990ee
cacigev <carl_simon.schultheis@tu-darmstadt.de>
2023-08-10 16:01:48 +0200

    Enclosed handle request

--- user.py
+++ user.py
@@ -5,8 +5,10 @@ import asyncio
 import logging
 import sys
 
-from bliss.comm.scpi import FuncCmd, ErrCmd, IntCmd, Commands
+from typing import Any
+# from bliss.comm.scpi import FuncCmd, ErrCmd, IntCmd, Commands
 from decouple import config
+import janus
 
 from wmControl.wavemeter import Wavemeter
 
@@ -61,56 +63,90 @@ async def decode_scpi(message: str):
     pass
 
 
-async def handle_request(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+async def read_stream(reader: asyncio.StreamReader, requests: janus.AsyncQueue):
+
+    pass
+
+
+async def create_wm_server(product_id: int, host: str, port: int) -> asyncio.Server:
     """
-    Handles client requests. Everytime called if a client connects to the server.
+    Creates a server for every connected wavemeter.
+
+    The handling for client requests is enclosed.
 
     Parameter
     ---------
-    reader : asyncio.StreamReader
-        Reader of the client.
-    writer : asyncio.StreamWriter
-        Writer of the client.
+    product_id: int
+        Version of the WM. Works like a serial number just not named like it.
+    host: str
+        IP-address of host.
+    port: int
+        Port-number to call and listen for a wavemeter. Specified through product_id.
     """
-    print('')
-    # Received requests.
-    while "EOF not reached":
-        data = await reader.readline()
-        if not data:  # <---------
-            break
+    async def handle_request(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+        """
+        Handles client requests. Everytime called if a client connects to the server.
 
-        message = data.decode().rstrip()
-        addr = writer.get_extra_info('peername')
-        print(f"Received {message!r} from {addr!r}")
+        Parameter
+        ---------
+        reader : asyncio.StreamReader
+            Reader of the client.
+        writer : asyncio.StreamWriter
+            Writer of the client.
+        """
+        client_requests: janus.Queue[str] = janus.Queue()  # Queue which gathers client requests.
+        measurements: janus.Queue[Any] = janus.Queue()  # Queue with answers for client.
+        tasks: set[asyncio.Task] = set()  # Set with TODOs.
+        # Received requests.
+        # ...
+        # read out stream from client
 
-        # Decode SCPI request.
-        coro = await decode_scpi(message)
+            # Decode SCPI request.
+            # coro = await decode_scpi(message)
 
         # Create new WM-object to answer the request.
-        # 4711: Quips B 192.168.1.240
-        # 536: WS-6
-        async with Wavemeter(4734, dll_path=dll_path) as ws8, Wavemeter(536, dll_path=dll_path) as ws6:
-            measurement = await ws8.get_wavelength(0)
+        # ...
+        # listen for answers
+        # get request with client_requests.get()
+        # put answer in measurements.put()
 
+        # Scream answers
+        # ...
+        # measurements.get() to stream with write
         print(f"Send: {measurement!r}")
         writer.write(str(measurement).encode())
         await writer.drain()
 
-    # Closing the connection.
-    print("Close the connection")
-    writer.close()
-    await writer.wait_closed()
+        await asyncio.gather(*tasks)  # Gather tasks and wait for them to be done.
 
+        # Closing the connection.
+        print("Close the connection")
+        writer.close()
+        await writer.wait_closed()
 
-async def main():
     server = await asyncio.start_server(
-        handle_request, '127.0.0.1', 8888)
-
+        handle_request, host, port)
     address = ', '.join(str(sock.getsockname()) for sock in server.sockets)
     print(f'Serving on {address}')
 
     async with server:
         await server.serve_forever()
+
+    return server
+
+
+async def main(wavemeter: [(int, int)]):
+    server_list: set[asyncio.Server] = set()
+    for wm, port in wavemeter:
+        server = await create_wm_server(wm, '127.0.0.1', port)
+
+        server_list.add(server)
+
+    await asyncio.gather(*server_list)
+
+
+    # async with server:
+    #     await server.serve_forever()
 
 
 logging.basicConfig(
@@ -121,6 +157,11 @@ logging.basicConfig(
 )
 
 try:
-    asyncio.run(main())
+    # 4711: Quips B 192.168.1.240
+    # 536: WS-6
+    wm_list = [(4734, 5555), (536, 5556)]
+    asyncio.run(main(wm_list))
 except KeyboardInterrupt:
+    pass
+finally:
     logging.getLogger(__name__).info("Application shut down.")
