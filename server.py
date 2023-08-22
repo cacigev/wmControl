c1278e3d7a9fd95ba7afbab854716a213aa4529d
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2023-08-22 02:38:51 +0200

    Updated wavemeter infos

--- server.py
+++ server.py
@@ -239,7 +239,8 @@ logging.basicConfig(
 
 try:
     # 4711: Quips B 192.168.1.240
-    # 536: WS-6
+    # 4734: WS-8 192.168.1.45
+    # 536: WS-6 192.168.1.45
     wm_list = [(4734, ("127.0.0.1", 5555)), (536, ("127.0.0.1", 5556))]
     asyncio.run(main(wm_list))
 except KeyboardInterrupt:
