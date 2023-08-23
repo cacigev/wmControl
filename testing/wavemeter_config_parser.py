dc2f31f607c9fccb1f8b2a443c2445cd5ff0cb7f
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2023-08-23 20:43:23 +0200

    Code cleanup

--- testing/wavemeter_config_parser.py
+++ testing/wavemeter_config_parser.py
@@ -12,21 +12,9 @@ from config_parser import parse_wavemeter_config
 @pytest.mark.parametrize(
     "wavemeter_config, expectation, result",
     [
-        (
-            '[[1000,"127.0.0.1",5555]]',
-            does_not_raise(),
-            [(1000, ip_interface("127.0.0.1"), 5555)],
-        ),
-        (
-            "[[1000,null,5555]]",
-            does_not_raise(),
-            [(1000, None, 5555)],
-        ),
-        (
-            "[[1000,null,5555], [1001,null,5556]]",
-            does_not_raise(),
-            [(1000, None, 5555), (1001, None, 5556)],
-        ),
+        ('[[1000,"127.0.0.1",5555]]', does_not_raise(), [(1000, ip_interface("127.0.0.1"), 5555)]),
+        ("[[1000,null,5555]]", does_not_raise(), [(1000, None, 5555)]),
+        ("[[1000,null,5555], [1001,null,5556]]", does_not_raise(), [(1000, None, 5555), (1001, None, 5556)]),
         ("[[abc,null,5555]]", pytest.raises(ValidationError), []),
         ('[[1000,"localhost",5555]]', pytest.raises(ValidationError), []),
         ('[[1000,"192.168.1.5",abc]]', pytest.raises(ValidationError), []),
